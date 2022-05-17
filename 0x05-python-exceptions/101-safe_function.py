7d4569ac73ba3b5b6507daf692a0a8a77b4f7cee
edgar <edgar-william@hotmail.com>
2022-05-17 04:04:04 +0000

    Safe function

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    import sys
+    try:
+        return fct(*args)
+    except Exception as msg:
+        print("Exception: {}".format(msg), file=sys.stderr)
+        return None
