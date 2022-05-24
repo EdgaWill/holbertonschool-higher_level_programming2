78c2e16f11e499f02865b5b5e6d90ed64abbed42
edgar <edgar-william@hotmail.com>
2022-05-24 05:08:35 +0000

    Eval is magic

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+""" You can create instances of objects related to geometric figures
+    Class:
+        Dimension
+        Rectangule
+"""
+
+
+class Dimension:
+    """ Define dimensions the Rectangle class
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise Exception("width must be an integer")
+        if value < 0:
+            raise Exception("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise Exception("height must be an integer")
+        if value < 0:
+            raise Exception("height must be >= 0")
+        self.__height = value
+
+
+class Rectangle(Dimension):
+    """ Define the Rectangle class
+    """
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        A = 2 * (self.width + self.height)
+        if self.width == 0 or self.height == 0:
+            A = 0
+        return A
+
+    def __str__(self):
+        pug = ""
+        if self.width == 0 or self.height == 0:
+            return pug
+        for e in range(self.height):
+            pug += '#' * self.width
+            if e != self.height - 1:
+                pug += '\n'
+        return pug
+
+    def __repr__(self):
+        return 'Rectangle(' + str(self.width) + ', ' + str(self.height) + ')'
