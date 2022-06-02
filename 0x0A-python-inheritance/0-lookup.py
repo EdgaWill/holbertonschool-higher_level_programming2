cf19cf1d4212183e79c73affbcd6987a2c71bf2e
edgar <edgar-william@hotmail.com>
2022-06-02 07:10:50 +0000

    Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""attributes and methods of an object
+    Class:
+        lookup
+"""
+
+
+def lookup(obj):
+    """ function definition that return a list object
+    """
+    return dir(obj)
