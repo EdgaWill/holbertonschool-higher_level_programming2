f208acbe54c17b6b828e2e5b30a08a4a3f0f02f9
EdgarWill <84213405+EdgaWill@users.noreply.github.com>
2022-07-07 15:44:29 -0500

    My genres

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- My genres
+SELECT a.name FROM tv_genres a
+INNER JOIN tv_show_genres
+ON a.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY a.name;
