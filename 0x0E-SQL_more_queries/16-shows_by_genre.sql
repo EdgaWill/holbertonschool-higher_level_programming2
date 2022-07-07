25d5789c94bfa38cfb3b2e98bd5c89f7adb0ffbf
EdgarWill <84213405+EdgaWill@users.noreply.github.com>
2022-07-07 15:46:24 -0500

    List shows and genres

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- List shows and genres
+SELECT s.title, g.name FROM tv_shows s
+LEFT JOIN tv_show_genres sg
+ON s.id = sg.show_id
+LEFT JOIN tv_genres g
+ON g.id = sg.genre_id
+ORDER BY s.title, g.name;
