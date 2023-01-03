03783d729988676e64460fc416b33d2da811e135
edgawill <edgar-william@hotmail.com>
2023-01-03 19:36:08 +0000

    class Square that defines a square and inherits

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -9,7 +9,7 @@ class Square extends Rectangle {
   charPrint (c) {
     if (c === undefined) c = 'X';
 
-    for (let a = 0; a < this.height; a++) {
+    for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.height));
     }
   }
