5f4eabb81274d202eea30b9f039a15df9fb1408f
edgawill <edgar-william@hotmail.com>
2023-01-03 21:44:17 +0000

    script that imports an array and computes a new array

--- javascript_objects_scopes_closures/100-map.js
+++ javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const { list } = require('./100-data');
 console.log(list);
-console.log(list.map((elemento, index) => elemento * index))
+console.log(list.map((elemento, index) => elemento * index));
