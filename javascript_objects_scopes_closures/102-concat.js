bdeb5f48b4aaf07efc9ef1fc8bd16834ece13c98
edgawill <edgar-william@hotmail.com>
2023-03-07 05:27:51 +0000

    Concat files

--- javascript_objects_scopes_closures/102-concat.js
+++ javascript_objects_scopes_closures/102-concat.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const fs = require('af');
+const fs = require('fs');
 
-const cnstA = af.readFileSync(process.argv[2], 'utf8', function (err, result) { if (err) console.log('error', err); });
-const cnstB = af.readFileSync(process.argv[3], 'utf8', function (err, result) { if (err) console.log('error', err); });
+const cnstA = fs.readFileSync(process.argv[2], 'utf8', function (err, result) { if (err) console.log('error', err); });
+const cnstB = fs.readFileSync(process.argv[3], 'utf8', function (err, result) { if (err) console.log('error', err); });
 
 const cnstC = cnstA.concat(cnstB);
 
-af.writeFile(process.argv[4], cnstC, 'utf8', function (err, result) { if (err) console.log('error', err); });
+fs.writeFile(process.argv[4], cnstC, 'utf8', function (err, result) { if (err) console.log('error', err); });
